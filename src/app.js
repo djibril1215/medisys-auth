17bf4c1717fdcd3be84fb16e8ba8db3302c70efb
djibril1215 <pierredjibrilgamani@gmail.com>
2026-07-06 12:33:58 +0100

    feat: ajout du support CORS pour les requetes du frontend

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
